0f1af78d8774bab54db810b0c261378f09ca918f
kimv06 <monalpodder12@gmail.com>
2022-12-04 21:31:39 +0530

    indexing

---
+++ Algorithms/num_insert_search.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int arr[50] , n = 0, index = -1 , key = 0;
+    cout<<"Enter size: ";
+    cin>>n;
+    for(int i = 0 ; i < n ; i++)
+    {
+        cout<<"Enter number: ";
+        cin>>arr[i];
+    }
+    cout<<"Enter key: ";
+    cin>>key;
+    for(int i = 0 ; i < n ; i++)
+    {
+        if(arr[i] == key)
+        {
+            index = i;
+            break;
+        }
+    }
+    if(index == -1)
+    {
+        n++;
+        for(int i = 0 ; i < n ; i++)
+        {
+            if(key>arr[i])
+            {
+                arr[i+1] = key;
+                index = i+1;
+            }
+        }
+    }
+    cout<<"index = "<<index;
+    return 0;
+}
