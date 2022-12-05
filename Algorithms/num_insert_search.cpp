e1fd8838a1fc36eab4bb707901316b40c69d140e
kimv06 <monalpodder12@gmail.com>
2022-12-05 12:39:02 +0530

    work@tech

--- Algorithms/num_insert_search.cpp
+++ Algorithms/num_insert_search.cpp
@@ -2,36 +2,40 @@
 using namespace std;
 int main()
 {
-    int arr[50] , n = 0, index = -1 , key = 0;
-    cout<<"Enter size: ";
-    cin>>n;
-    for(int i = 0 ; i < n ; i++)
+    int arr[50], n = 0, index = -1, key = 0;
+    cout << "Enter size: ";
+    cin >> n;
+    for (int i = 0; i < n; i++)
     {
-        cout<<"Enter number: ";
-        cin>>arr[i];
+        cout << "Enter number: ";
+        cin >> arr[i];
     }
-    cout<<"Enter key: ";
-    cin>>key;
-    for(int i = 0 ; i < n ; i++)
+    cout << "Enter key: ";
+    cin >> key;
+    for (int i = 0; i < n; i++)
     {
-        if(arr[i] == key)
+        if (arr[i] == key)
         {
             index = i;
             break;
         }
     }
-    if(index == -1)
+    if (index == -1)
     {
         n++;
-        for(int i = 0 ; i < n ; i++)
+        for (int i = 0; i < n; i++)
         {
-            if(key>arr[i])
+            if (key > arr[i])
             {
-                arr[i+1] = key;
-                index = i+1;
+                index = i + 1;
+            }
+            else if (key < arr[i])
+            {
+                index = i;
+                break;
             }
         }
     }
-    cout<<"index = "<<index;
+    cout << "index = " << index;
     return 0;
 }
